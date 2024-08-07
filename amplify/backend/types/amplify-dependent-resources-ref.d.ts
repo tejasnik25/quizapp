9e19d2c37cd04c072825619fb0d17eb2d5069613
tejasnik25 <tejasnikam2505@gmail.com>
2024-08-07 15:57:19 +0530

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "quizappb583d4fb": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/quizData.js
@@ -0,0 +1,55 @@
+const quizData = [
+    {
+      question: "Which technology is used to create a virtual representation of a physical object or environment?",
+      options: ["Artificial Intelligence (AI)", "Virtual Reality (VR)", "Internet of Things (IoT)", "Cloud Computing"],
+      answer: "Virtual Reality (VR)"
+    },
+    {
+      question: "What is the primary purpose of a firewall in computer networks?",
+      options: ["To increase internet speed", "To protect against unauthorized access", "To store data", "To monitor network performance"],
+      answer: "To protect against unauthorized access"
+    },
+    {
+      question: "Which programming language is known for its use in web development and is often used for client-side scripting?",
+      options: ["Python", "Java", "JavaScript", "C++"],
+      answer: "JavaScript"
+    },
+    {
+      question: "What does the acronym HTTP stand for in web technology?",
+      options: ["HyperText Transfer Protocol", "Hyperlink Transfer Technology Protocol", "Hyperlink Text Transfer Protocol", "HyperText Technology Protocol"],
+      answer: "HyperText Transfer Protocol"
+    },
+    {
+      question: "Which programming language is known as the backbone of the World Wide Web?",
+      options: ["Java", "C#", "Python", "HTML"],
+      answer: "HTML"
+    },
+    {
+      question: "In computing, what does AI stand for?",
+      options: ["Automated Intelligence", " Artificial Integration", "Artificial Intelligence", "Advanced Interface"],
+      answer: "Artificial Intelligence"
+    },
+    {
+      question: "Which technology is used to enhance the user's interaction with real-world objects through digital overlays?",
+      options: ["Augmented Reality (AR)", "Virtual Reality (VR)", "Machine Learning", "Internet of Things (IoT)"],
+      answer: "Augmented Reality (AR)"
+    },
+    {
+      question: "What is the primary function of a database management system (DBMS)?",
+      options: ["To design websites", "To manage and organize data", " To secure networks", "To develop mobile applications"],
+      answer: "To manage and organize data"
+    },
+    {
+      question: "Which of these companies was not founded in a garage?",
+      options: ["Amazon", "Google", "Apple", "Microsoft"],
+      answer: "Amazon"
+    },
+    {
+      question: "What does 'GPU' stand for?",
+      options: ["Graphical Processing Unit", "Graphics Performance Unit", "Graphics Processing Unit", "Graphical Performance Unit"],
+      answer: "Graphics Processing Unit"
+    },  
+  ];
+  
+  export default quizData;
+  
